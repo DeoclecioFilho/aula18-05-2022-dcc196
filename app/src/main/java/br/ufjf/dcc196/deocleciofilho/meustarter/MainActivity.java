a2f509a4ffab5cc643a170d0471741e8ade1d37e
deocleciofilho <deoclecio.filho@estudante.ufjf.br>
2022-05-18 22:46:13 -0300

    Adicionado saída com base na entrada.

--- app/src/main/java/br/ufjf/dcc196/deocleciofilho/meustarter/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/deocleciofilho/meustarter/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        EditText editTestNome = findViewById(R.id.editTextNome);
+        EditText editTestStarter = findViewById(R.id.editTextStarter);
     }
 
     public void clicaBotaoMensagem(View origem){
         TextView textViewMensagem = findViewById(R.id.textViewMensagemP);
-        textViewMensagem.setText("Olá mundo!");
+      //  textViewMensagem.setText("Olá mundo!");
+        EditText editTestNome = findViewById(R.id.editTextNome);
+        EditText editTestStarter = findViewById(R.id.editTextStarter);
+
+        //trabalha os dados
+        String nome = editTestNome.getText().toString();
+        String starter = editTestStarter.getText().toString();
+        String mensagem = String.format("Olá treinador %s! Seu starter é %s!", nome, starter);
+
+        // Apresenta resultado
+        textViewMensagem.setText(mensagem);
 
     }
 }
